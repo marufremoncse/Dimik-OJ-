7988426858fa00f01b793cf06a8f8ad2ea2e816a
marufremoncse <50164327+marufremoncse@users.noreply.github.com>
2020-07-15 12:45:20 +0600

    Update [৫২ সমস্যা বই] ছোট থেকে বড়.cpp

--- [৫২ সমস্যা বই] ছোট থেকে বড়.cpp
+++ [৫২ সমস্যা বই] ছোট থেকে বড়.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin>>n;
-    int ara[n][3];
-    for(int i=0;i<n;i++){
+int main(){
+    int T;
+    cin>>T;
+
+    int ara[T][3];
+
+    for(int i=0;i<T;i++){
         cin>>ara[i][0]>>ara[i][1]>>ara[i][2];
     }
-    for(int i=0;i<n;i++){
+
+    for(int i=0;i<T;i++){
         sort(ara[i],ara[i]+3);
         cout<<"Case "<<i+1<<": "<<ara[i][0]<<" "<<ara[i][1]<<" "<<ara[i][2]<<endl;
     }
