36416a78ccf3da3f1de211cddc59cff94c40191c
marufremoncse <50164327+marufremoncse@users.noreply.github.com>
2020-07-18 12:35:12 +0600

    Update [৫২ সমস্যা বই] রান রেট - ১.cpp

--- [৫২ সমস্যা বই] রান রেট - ১.cpp
+++ [৫২ সমস্যা বই] রান রেট - ১.cpp
@@ -1,25 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin>>n;
+int main(){
+    int T;
+    cin>>T;
 
-    double ara[n][3];
+    double ara[T][3];
 
-    for(int i=0;i<n;i++){
+    for(int i=0;i<T;i++){
         cin>>ara[i][0]>>ara[i][1]>>ara[i][2];
     }
-    for(int i=0;i<n;i++){
-        double curr = (ara[i][1]*6)/(300-ara[i][2]);
-        printf("%.2f",curr);
-        double requ;
-        if(ara[i][0]<ara[i][1])
-            requ=0.00;
-        else{
-            requ = ((ara[i][0]-ara[i][1]+1)*6)/(ara[i][2]);
+
+    double current_run_rate, required_run_rate;
+    for(int i=0;i<T;i++){
+        current_run_rate = ara[i][1]/((300-ara[i][2])/6);
+
+        if(ara[i][0]<ara[i][1]){
+            required_run_rate = 0;
         }
-        printf(" %.2f\n",requ);
+        else{
+            required_run_rate = (ara[i][0]-ara[i][1]+1)/(ara[i][2]/6);
+        }
+        printf("%.2f %.2f\n",current_run_rate,required_run_rate);
     }
 }
