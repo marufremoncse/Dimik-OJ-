91edf0c1ca6bc17ff74e36c57b6224b8b56ba5f0
marufremoncse <50164327+marufremoncse@users.noreply.github.com>
2020-07-20 18:33:52 +0600

    Update [৫২ সমস্যা বই] গৌণিক,ফ্যাক্টরিয়াল.cpp

--- [৫২ সমস্যা বই] গৌণিক,ফ্যাক্টরিয়াল.cpp
+++ [৫২ সমস্যা বই] গৌণিক,ফ্যাক্টরিয়াল.cpp
@@ -1,40 +1,23 @@
 #include<bits/stdc++.h>
-#define MAX 200
 using namespace std;
 
-int ara[MAX];
+int main(){
+    int T;
+    cin>>T;
 
-int main()
-{
-    int n;
-    cin>>n;
-    int t = 0;
-    int input[n];
-    for(int i=0;i<n;i++){
-        cin>>input[i];
+    int ara[T];
+
+    for(int i=0;i<T;i++){
+        cin>>ara[i];
     }
-    while(t<n){
-        int p = input[t];
-        int ara[MAX]={0};
-        ara[0] = 1;
-        int count = 0;
-        for(;p>=2;p--){
-            int temp=0;
-            for(int j=0;j<=count;j++){
-                temp = (ara[j]*p) + temp;
-                ara[j] = temp % 10;
-                temp = temp / 10;
-            }
-            while(temp>0){
-                ara[++count] = temp % 10;
-                temp = temp / 10;
-            }
+
+    long long output;
+    for(int i=0;i<T;i++){
+        output = 1;
+
+        for(int j=1;j<=ara[i];j++){
+            output = output * j;
         }
-        for(int k=count;k>=0;k--){
-            cout<<ara[k];
-        }
-        cout<<endl;
-        t++;
+        cout<<output<<endl;
     }
 }
-
